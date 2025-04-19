bbe36181e5db82db2cce8740767c6eafe9615288
Christopher <contato.christopher@gmail.com>
2025-04-18 21:02:46 -0300

    test: create new tests for transactions

--- 02-api-rest-nodejs/src/env/index.ts
+++ 02-api-rest-nodejs/src/env/index.ts
@@ -1,10 +1,16 @@
-import "dotenv/config";
-import { z } from "zod";
+import { config } from 'dotenv';
+import { z } from 'zod';
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' });
+} else {
+  config({ path: '.env' });
+}
 
 const envSchema = z.object({
-	NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
-	DATABASE_URL: z.string(),
-	PORT: z.coerce.number().default(3333),
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
 });
 
 // O parse vai verificar se as variáveis de ambiente estão de acordo com o schema
@@ -12,9 +18,9 @@ const envSchema = z.object({
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
-	console.error("⚠️ Invalid environment variables", _env.error.format());
+  console.error('⚠️ Invalid environment variables', _env.error.format());
 
-	throw new Error("Invalid environment variables.");
+  throw new Error('Invalid environment variables.');
 }
 
 export const env = _env.data;
--- 02-api-rest-nodejs/src/tests/transactions.spec.ts
+++ 02-api-rest-nodejs/src/tests/transactions.spec.ts
@@ -1,6 +1,15 @@
-import { afterAll, beforeAll, describe, it, test } from 'vitest';
+import {
+  afterAll,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+  test,
+} from 'vitest';
 import request from 'supertest';
 import { app } from '../app';
+import { exec, execSync } from 'node:child_process';
 
 describe('Transactions', () => {
   beforeAll(async () => {
@@ -11,7 +20,17 @@ describe('Transactions', () => {
     await app.close();
   });
 
-  it('shoudl be able to create a new transaction', async () => {
+  beforeEach(async () => {
+    console.log('Rolling back migrations...');
+    execSync('npm run knex migrate:rollback --all');
+
+    console.log('Running migrations...');
+    execSync('npm run knex migrate:latest');
+  });
+
+  // e2e test - são poucos, mas são importantes para garantir que a aplicação funcione como um todo
+
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -20,5 +39,69 @@ describe('Transactions', () => {
         type: 'credit',
       })
       .expect(201);
+  });
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transação',
+        amount: 55000,
+        type: 'credit',
+      })
+      .expect(201);
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    console.log('Cookies:', cookies);
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+      .expect(200);
+
+    console.log(listTransactionsResponse.body.transaction);
+
+    expect(listTransactionsResponse.body.transaction).toEqual([
+      expect.objectContaining({
+        amount: 55000,
+        title: 'Nova transação',
+      }),
+    ]);
+  });
+
+  it('should be able to list a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transação',
+        amount: 55000,
+        type: 'credit',
+      })
+      .expect(201);
+
+    const cookies = createTransactionResponse.get('Set-Cookie');
+
+    console.log('Cookies:', cookies);
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+      .expect(200);
+
+    const transactionId = listTransactionsResponse.body.transaction[0].id;
+    console.log('Transaction ID:', transactionId);
+
+    const getTransactionResponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies ?? [])
+      .expect(200);
+
+    expect(getTransactionResponse.body).toEqual(
+      expect.objectContaining({
+        amount: 55000,
+        title: 'Nova transação',
+      }),
+    );
   });
 });
