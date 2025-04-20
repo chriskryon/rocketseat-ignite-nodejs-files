359104738868a534779a01ea611024e7774981e8
Christopher <contato.christopher@gmail.com>
2025-04-19 22:46:21 -0300

    feat: add env validation

--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,8 +1,11 @@
+import { env } from "../env";
 import { app } from "./app";
 
-app.listen({
-    host: '0.0.0.0',
-    port: 3000 }
-    ).then(() => {
-        console.log('HTTP Server running on 3333');
-    });
+app
+	.listen({
+		host: "0.0.0.0",
+		port: env.PORT,
+	})
+	.then(() => {
+		console.log("HTTP Server running on 3333");
+	});
